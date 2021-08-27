1f8de2e747b943921e558c4ba24c5469a240b570
Steven <sshropshire@paypal.com>
2021-08-27 11:03:00 -0500

    Check if coroutine is active before resuming.

--- CardKtx/src/main/java/com/braintreepayments/api/CardExtensions.kt
+++ CardKtx/src/main/java/com/braintreepayments/api/CardExtensions.kt
@@ -1,17 +1,22 @@
 package com.braintreepayments.api
 
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.NonCancellable.isActive
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
+@InternalCoroutinesApi
 suspend fun CardClient.tokenize(card: Card) =
     suspendCancellableCoroutine<CardNonce> { continuation ->
         tokenize(card) { cardNonce, error ->
-            cardNonce?.let {
-                continuation.resume(it)
-            }
-            error?.let {
-                continuation.resumeWithException(it)
+            if (isActive) {
+                cardNonce?.let {
+                    continuation.resume(it)
+                }
+                error?.let {
+                    continuation.resumeWithException(it)
+                }
             }
         }
     }
