ce48f25e9558f7d690fcd695f80f260614f85634
Steven <sshropshire@paypal.com>
2021-08-27 13:41:28 -0500

    Add build.gradle for card extensions module and check if coroutine is active before continuing.

--- CardKtx/src/main/java/com/braintreepayments/api/CardExtensions.kt
+++ CardKtx/src/main/java/com/braintreepayments/api/CardExtensions.kt
@@ -1,16 +1,13 @@
 package com.braintreepayments.api
 
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.NonCancellable.isActive
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
-@InternalCoroutinesApi
 suspend fun CardClient.tokenize(card: Card) =
     suspendCancellableCoroutine<CardNonce> { continuation ->
         tokenize(card) { cardNonce, error ->
-            if (isActive) {
+            if (continuation.isActive) {
                 cardNonce?.let {
                     continuation.resume(it)
                 }
