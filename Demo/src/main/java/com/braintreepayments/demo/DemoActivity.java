e5f7599cfcbef26d768a9f40cf190ed5d1f7d76a
sshropshire <sshropshire@paypal.com>
2022-03-11 10:14:37 -0600

    Cleanup DemoActivity unused variable.

--- Demo/src/main/java/com/braintreepayments/demo/DemoActivity.java
+++ Demo/src/main/java/com/braintreepayments/demo/DemoActivity.java
@@ -30,7 +30,6 @@ import java.util.List;
 
 public class DemoActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback, ActionBar.OnNavigationListener {
 
-    private BraintreeClient braintreeClient;
     private AppBarConfiguration appBarConfiguration;
 
     @Override
@@ -103,7 +102,6 @@ public class DemoActivity extends AppCompatActivity implements ActivityCompat.On
 
     private void performReset() {
         setProgressBarIndeterminateVisibility(true);
-        braintreeClient = null;
     }
 
     public void showDialog(String message) {
